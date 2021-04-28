8b244928a466634ab623e675c2da1278ad9a59e9
SusmitoB <susmito.bhattacharyya@gmail.com>
2021-04-28 19:58:34 +0530

    Update 1

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Error = () => {
     return (
         <div className="error">
-            Error occured
+            Wrong URL!!! Please check the url.
         </div>
     )
 }
